5299a2c684c89b37fd3828395fdf940dd2f6f1ba
Shyam050802 <112692432+Shyam050802@users.noreply.github.com>
2023-09-13 19:03:23 +0530

    Update and rename new.py to chatGPT_API.py

--- chatGPT_API.py
+++ chatGPT_API.py
@@ -1,6 +1,6 @@
 import openai
 from apikey import APIKEY
-openai.api_key = APIKEY;
+openai.api_key = APIKEY;//Enter your API key here
 
 output = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
